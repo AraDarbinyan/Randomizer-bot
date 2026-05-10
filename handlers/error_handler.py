a3acac4946816d6e3b12e24d428ffff44bb706b9
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2026-05-10 21:00:48 +0400

    added error handler

---
+++ handlers/error_handler.py
@@ -0,0 +1,22 @@
+from telegram.ext import ContextTypes
+from services.users import get_user_language
+from bot_handlers import t
+import logging
+
+logger = logging.getLogger(__name__)
+
+async def error_handler(update: object, context: ContextTypes.DEFAULT_TYPE):
+    logger.error("Exception while handling update:", exc_info=context.error)
+
+    if update and hasattr(update, "effective_user"):
+        user_id = update.effective_user.id
+
+        try:
+            lang = await get_user_language(user_id)
+        except:
+            lang = "ru"
+
+        try:
+            await update.effective_message.reply_text(t(lang, "error"))
+        except:
+            pass
