0487c372751ceea9d51a188d64818c8c2ea01c2d
Ara Darbinyan <aradarbinyan.2003@gmail.com>
2026-08-27 14:44:52 +0400

    fix: use async PostgreSQL URL in Alembic

--- migrations/env.py
+++ migrations/env.py
@@ -9,14 +9,14 @@ from alembic import context
 from dotenv import load_dotenv
 from db.models import Base
 from db import models
+from config import DATABASE_URL
 import os
 
 load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-database_url = os.getenv("DATABASE_URL")
-config.set_main_option("sqlalchemy.url", database_url)
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
